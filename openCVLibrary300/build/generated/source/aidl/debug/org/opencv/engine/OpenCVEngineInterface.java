cba20d3d0b4188ac5e145528522dc55827b7cf59
grassmoon <lokywong.hk@gmail.com>
2016-11-27 22:09:48 +0800

    Merge branch 'master' of C:\Users\lokyw\AndroidProject\TryOnGlassesCust with conflicts.

--- openCVLibrary300/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary300/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\yingc\\Desktop\\CSIT5110TryOnGlass\\TryOnGlassesCust\\openCVLibrary300\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\lokyw\\AndroidProject\\TryOnGlassesCust\\openCVLibrary300\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
